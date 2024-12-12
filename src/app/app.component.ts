a20c62f886bd126b9f703a43dd8566a1cb3d160e
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2024-12-13 00:33:20 +0200

    set up project structure & environment and call first api (get products) in app component by using ts types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    
-  ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: Product[]=[];
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(environment.apiUrl + 'products').subscribe({
+      next: response => {this.products= response.items;
+        console.log(this.products);
+      },
+      error: error => console.log(error),
+      complete: () => console.log('Request Completed')
+    });
+  }
 }
