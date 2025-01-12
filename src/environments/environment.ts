d5048d70ba388aa9d5b8d29761dcfc3ad5bc40a8
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2025-01-12 12:19:37 +0200

    edit configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:44328/',
-    hubUrl: 'https://localhost:44328/hub/notifications',
+    apiUrl: '/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51Oij4JE1HorgB094IRzUgDdZE5uJp8nXzWEROwB2XJ4UqrzuTJtcGizcDKGhxu5hEtF6zC2KvbZp6TOdhaZ7cc7400KemoESEU'
 };
